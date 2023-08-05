6bf55fb5785cef62b06c7faa8c07c483d8a9fa4f
spencer pope <34104632+spope851@users.noreply.github.com>
2023-08-04 22:51:33 -0600

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,27 @@
-import { NextPage } from 'next'
-import { LandingPage } from './components/main'
+"use client";
+import { useRouter } from "next/navigation";
+import Landing from "./components/landing";
+import { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <LandingPage />
-}
+  const router = useRouter();
+  return (
+    <Landing
+      title="investor & entrepreneur"
+      name={process.env.NEXT_PUBLIC_NAME || "not found"}
+      subtitle="Building an unconventional life"
+      socialUrls={[
+        "https://twitter.com/s_pop3",
+        "https://github.com/spope851",
+        "mailto:spenpo@spenpo.com",
+        "https://www.youtube.com/@spope",
+        "https://www.twitch.tv/spenpo",
+      ]}
+      headshotSrc="/headshot.jpeg"
+      actionClick={() => router.push("/demo")}
+      actionText="get a landing page like this one"
+    />
+  );
+};
 
-export default Home
+export default Home;
