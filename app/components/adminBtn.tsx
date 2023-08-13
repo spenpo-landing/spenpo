e4e2094f2320ae2688d96a92ac7a2180f5e711e0
spope <spope@blockchains.com>
2023-08-13 13:52:42 -0600

    integrate nextauth with env variables and admin button

--- app/components/adminBtn.tsx
+++ app/components/adminBtn.tsx
@@ -1,47 +1,37 @@
-'use client'
-import React from 'react'
-import { IconButton } from '@mui/material'
-import { useSession } from 'next-auth/react'
-import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings'
-import LogoutIcon from '@mui/icons-material/Logout'
-import PendingIcon from '@mui/icons-material/Pending'
+import React from "react";
+import { IconButton } from "@mui/material";
+import { useSession } from "next-auth/react";
+import AdminPanelSettingsIcon from "@mui/icons-material/AdminPanelSettings";
+import LogoutIcon from "@mui/icons-material/Logout";
+import PendingIcon from "@mui/icons-material/Pending";
 
 const config = {
   unauthenticated: {
     icon: <AdminPanelSettingsIcon />,
-    href: '/api/auth/signin?callbackUrl=/',
-    opacity: 0.5,
+    href: "/api/auth/signin",
   },
   authenticated: {
     icon: <LogoutIcon />,
-    href: '/api/auth/signout?callbackUrl=/',
-    opacity: 1,
+    href: "/api/auth/signout",
   },
   loading: {
     icon: <PendingIcon />,
-    href: '/',
-    opacity: 0.5,
+    href: "/",
   },
-}
+};
 
 const AdminBtn: React.FC = () => {
-  const session = useSession()
-  const hideAdmin = JSON.parse(process.env.NEXT_PUBLIC_HIDE_ADMIN || '')
-
-  if (hideAdmin && session.status !== 'authenticated') return <></>
-
+  const session = useSession();
   return (
-    <IconButton
-      href={config[session.status].href}
-      sx={{
-        position: 'absolute',
-        right: session.status === 'authenticated' ? 80 : 0,
-        opacity: config[session.status].opacity,
-      }}
-    >
-      {config[session.status].icon}
-    </IconButton>
-  )
-}
+    !JSON.parse(process.env.NEXT_PUBLIC_HIDE_ADMIN || "") && (
+      <IconButton
+        href={config[session.status].href}
+        sx={{ position: "absolute", right: 0, opacity: 0.5 }}
+      >
+        {config[session.status].icon}
+      </IconButton>
+    )
+  );
+};
 
-export default AdminBtn
+export default AdminBtn;
