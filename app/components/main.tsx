d2b04e99ce623654bacbe89aea6d173ea434159b
spope <spope@blockchains.com>
2023-08-31 17:52:44 -0600

    export static metadata object

--- app/components/main.tsx
+++ app/components/main.tsx
@@ -1,30 +1,37 @@
-'use client'
-import { useSession } from 'next-auth/react'
-import React, { useContext, useState } from 'react'
-import { CmsContext } from '../context/cms'
-import AdminBtn from './adminBtn'
-import { TopComponents } from './topComponents'
-import dynamic from 'next/dynamic'
-
-const SpenpoLanding = dynamic(
-  () => import('spenpo-landing').then((res) => res.SpenpoLanding),
-  {
-    ssr: false,
-  }
-)
+"use client";
+import { Button } from "@mui/material";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import React, { useContext, useState } from "react";
+import { CmsContext } from "../context/cms";
+import AdminBtn from "./adminBtn";
+import Landing from "./landingPage";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 
 export const LandingPage: React.FC = () => {
-  const editable = useState(false)
-  const session = useSession()
-  const { landingCms } = useContext(CmsContext)
+  const editable = useState(false);
+  const session = useSession();
+  const router = useRouter();
+  const { landingCms } = useContext(CmsContext);
   return (
     <>
       <AdminBtn />
-      <SpenpoLanding
+      <Landing
         cms={landingCms}
-        editable={session.status === 'authenticated' ? editable : undefined}
-        topComponents={<TopComponents editable={editable[0]} />}
+        editable={session.status === "authenticated" ? editable : undefined}
+        topComponents={
+          session.status === "authenticated" ? (
+            <Button
+              onClick={() => router.push("/admin")}
+              variant="contained"
+              sx={{ ml: "auto", mr: 3 }}
+              endIcon={<ChevronRightIcon />}
+            >
+              review changes
+            </Button>
+          ) : undefined
+        }
       />
     </>
-  )
-}
+  );
+};
